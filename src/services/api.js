26eb79ea31766477ea5ed8413b12b3a71035a8c6
Victor Oluwapelumi <victoroluwadarasimi16@gmail.com>
2025-06-30 23:21:57 +0100

    correction commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
 
-// const BASE_URL = 'https://showmar-backend.onrender.com/api'
-
-// const api = axios.create({
-//   baseURL: BASE_URL,
-//   withCredentials: true, // only if you're using cookies or sessions
-// });
+const BASE_URL = 'https://showmar-backend.onrender.com/api'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-})
+  baseURL: BASE_URL,
+  withCredentials: true, // only if you're using cookies or sessions
+});
+
+// const api = axios.create({
+//   baseURL: 'http://localhost:5000/api',
+// })
 
 export const setAuthToken = (token) => {
   if (token) {
