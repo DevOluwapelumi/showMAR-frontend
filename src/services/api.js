105969ee959675efccf44beecbbe001c977073a9
Victor Oluwapelumi <victoroluwadarasimi16@gmail.com>
2025-06-30 02:38:35 +0100

    PWA commit

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const BASE_URL = 'https://showmar-backend.onrender.com/api'
 const api = axios.create({
   baseURL: BASE_URL,
   withCredentials: true, // only if you're using cookies or sessions
-})
+});
 
 // const api = axios.create({
 //   baseURL: 'http://localhost:5000/api',
